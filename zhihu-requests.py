a0e120a7d17eb3349c49be5a1cac9b3d6bfa5745
chaasse <32757323+chaasse@users.noreply.github.com>
2018-04-14 11:48:22 +0800

    Update zhihu-requests.py

--- zhihu-requests.py
+++ zhihu-requests.py
@@ -1,7 +1,7 @@
 # -*- coding:UTF-8 -*-
 
 import requests, time
-import hmac, json #这几个可以从js中获取sha1从而知道这个
+import hmac, json  #这几个需要导入的可以从js中获取，加密需要的
 from bs4 import BeautifulSoup
 from hashlib import sha1
 
@@ -9,7 +9,7 @@ def get_signature(grantType, clientId, source, timestamp):
     ''' 处理签名 '''
 
     hm = hmac.new(b'd1b964811afb40118a12068ff74a12f4', None, sha1) # 这个数字在js中查找
-    hm.update(str.encode(grantType)) # 括号中是转为字节  这几个的顺序  这个顺序在js中是固定的
+    hm.update(str.encode(grantType)) # 括号中是转为字节  这几个的顺序  js中指定的顺序，不可以更改
     hm.update(str.encode(clientId))
     hm.update(str.encode(source))
     hm.update(str.encode(timestamp))
@@ -19,12 +19,10 @@ def get_signature(grantType, clientId, source, timestamp):
 
 def login(username, password, s,header):
     ''' 处理login '''
-    resp2 = s.get('https://www.zhihu.com/api/v3/oauth/captcha?lang=cn', headers=header)  # 拿cookie:capsion_ticket
     grantType = 'password'
     clientId = 'c3cef7c66a1843f8b3a9e6a1e3160e20'
     source = 'com.zhihu.web'
     timestamp = str(int(time.time()*1000))
-   
     resp2 = s.get('https://www.zhihu.com/api/v3/oauth/captcha?lang=cn', headers=header) # 拿cookie
     data = {
         "client_id": clientId,
@@ -56,7 +54,9 @@ if __name__ == "__main__":
                # 通过看headers中的数据，可以把感觉有用的都加上
                }
 
-    login('12345', '89106892', session, headers)  # 用户名密码换自己的就好了
-    resp = session.get('https://www.zhihu.com/inbox', headers=headers) 
+    login('1833980716', '89108920.com', session, headers)  # 用户名密码换自己的就好了
+    resp = session.get('https://www.zhihu.com/inbox', headers=headers)  # 登录进去了
+    print(BeautifulSoup(resp.content, 'html.parser'))
     print(resp.cookies.items())
     print(resp.cookies)
+
